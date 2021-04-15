db808786afe6e47d60896bde599da31e9ce948b7
ekulbyrnes <luke.e.byrnes@gmail.com>
2021-04-15 12:51:27 +1000

    language settings specified

--- ras_site/settings.py
+++ ras_site/settings.py
@@ -123,6 +123,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
+LANGUAGES = [
+    ('en', 'English'),
+]
+
 LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Australia/Melbourne'
