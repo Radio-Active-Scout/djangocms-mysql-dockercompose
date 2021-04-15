3c970d56b529f2aa9d807edae4d0dc987c18774f
ekulbyrnes <luke.e.byrnes@gmail.com>
2021-04-15 12:49:19 +1000

    added django_admin_style

--- ras_site/settings.py
+++ ras_site/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_admin_style',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
